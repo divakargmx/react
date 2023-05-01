d3b4581773349c24d8fc4f2a6d52a9ec38f7a638
Divakar <132250823+divakargmx@users.noreply.github.com>
2023-05-01 12:25:55 +0530

    Update Info.js

--- src/info/Info.js
+++ src/info/Info.js
@@ -23,9 +23,9 @@ of what each of the values mean.
  */
 
 export const info = {
-    firstName: "John",
-    lastName: "Smith",
-    initials: "js", // the example uses first and last, but feel free to use three or more if you like.
+    firstName: "Divakar Rao",
+    lastName: "Potnuru",
+    initials: "js, Python", // the example uses first and last, but feel free to use three or more if you like.
     position: "a Full Stack Developer",
     selfPortrait: self, // don't change this unless you want to name your self-portrait in the "img" folder something else!
     gradient: `-webkit-linear-gradient(135deg, ${colors})`, // don't change this either
@@ -37,15 +37,15 @@ export const info = {
         },
         {
             emoji: '🌎',
-            text: 'based in the US'
+            text: 'based in the INDIA'
         },
         {
             emoji: "💼",
-            text: "Systems Engineer at Google"
+            text: "Medical Representative"
         },
         {
             emoji: "📧",
-            text: "johnsmith@gmail.com"
+            text: "divakar@gmx.us"
         }
     ],
     socials: [
