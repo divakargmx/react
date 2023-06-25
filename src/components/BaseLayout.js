b16638e25f89e5c90807a629846d93872b13271e
Divakar <132250823+divakargmx@users.noreply.github.com>
2023-06-25 09:11:46 +0530

    Update BaseLayout.js

--- src/components/BaseLayout.js
+++ src/components/BaseLayout.js
@@ -4,6 +4,8 @@ import Navbar from "./Navbar";
 import Home from "./home/Home";
 import About from "./about/About";
 import Portfolio from "./portfolio/Portfolio";
+import LoginPage from './LoginPage';
+import Dashboard from './Dashboard';
 import {Route, Routes} from "react-router-dom";
 import {Box, Grid} from "@mui/material";
 
@@ -39,6 +41,7 @@ export default function BaseLayout() {
                   <Route exact path={'/'} element={<Home/>}/>
                   <Route exact path={'/about'} element={<About/>}/>
                   <Route exact path={'/portfolio'} element={<Portfolio/>}/>
+                  <Route exact path={'/LoginPage'} element={<LoginPage/>}/>
                </Routes>
             </Grid>
             <Grid item>
